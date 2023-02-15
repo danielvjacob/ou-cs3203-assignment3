a176a720419e7be0285e737def87da2a9d45f2ba
danielvjacob <98068637+danielvjacob@users.noreply.github.com>
2023-02-15 15:59:42 -0600

    added Sum method

---
+++ Assignment_Three.java
@@ -0,0 +1,14 @@
+package ou_CS3203;
+import java.util.*;
+
+public class Assignment_Three {
+    
+    public int Sum(int[] array) {
+        int result = 0;
+        for (int i = 0; i < array.length; i++){
+            result += array[i];
+        }
+        return result;
+    }
+
+}
